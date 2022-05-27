cf9e9bc6e6c5a3bc65934097f1a522fc7f867a08
hapid <hafid.kusuma45@gmail.com>
2022-05-27 15:38:01 +0700

    penambahan fungsi/1

---
+++ fungsi.py
@@ -0,0 +1,10 @@
+# Cara membuat suatu fungsi
+# 27 Mei 2022
+
+def selamat_datang():
+    print('selamat datang')
+    print('halo')
+    print('nama saya hapid')
+
+selamat_datang()
+selamat_datang()
